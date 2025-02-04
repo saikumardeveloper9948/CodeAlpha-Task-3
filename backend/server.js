c40ba4e110db7dd3cd9bf9294351f7eab41df799
saikumardeveloper9948 <bandisaikumar9948@gmail.com>
2025-02-04 18:22:21 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(cors({
-    origin: '*',  
+    origin: `http://127.0.0.1:${port}`,  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
   }));
